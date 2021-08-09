564d0f191689840970c9b36877549612ceb1dfd3
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-08-09 13:22:51 +0530

    final commit

---
+++ src/auth.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from "express"
+import config from "./config";
+import jwt from "jsonwebtoken";
+import { User } from "./Types";
+export let refreshTokens = new Map()
+
+/**
+ * 
+ * generates temp access tokens
+ * 
+ * @param user :  {
+    name: string;
+    email: string;
+    userID: string;
+}
+ * @returns jwt access token
+ */
+
+export const generateAuthToken = async (user : User) : Promise<string> => {
+    return jwt.sign(user, config.ACCESS_TOKEN_SECRET, {expiresIn : '1m'})
+}
+
+/**
+ * 
+ * @param req : Express.Request
+ * @param res : Express.Response
+ * @param next : Express.NextFunction
+ * @returns : HTTP Status Code : 401 : Unauthorized if not logged in, else moves on next()
+ */
+
+export const isLoggedIn = async(req: Request, res : Response, next : NextFunction)=>{
+const authHeader = req.headers["authorization"]
+const token = authHeader && authHeader.split(" ")[1]
+if(!token) return res.sendStatus(401)
+jwt.verify(token, config.ACCESS_TOKEN_SECRET,(err,user : User)=>{
+    if (err) {return res.status(403).json({message: "token has expired, generate a new token"})}
+    req.user = user
+    next()
+})
+}
+
+/**
+ * 
+ * @param user :  {
+    name: string;
+    email ?: string;
+    userID: string;
+}
+ * @returns : {accessToken : string, refreshToken : string}
+ */
+
+export const generateTokens = async(user : User) : Promise<{accessToken : string, refreshToken : string}>=>{
+    const accessToken = await  generateAuthToken(user);
+    const refreshToken = await jwt.sign(user, config.REFRESH_TOKEN_SECRET);
+    refreshTokens.set(refreshToken,refreshToken)
+    return {accessToken : accessToken, refreshToken :refreshToken}
+}
--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,6 @@ export default {
   DB_NAME: process.env.DB_NAME,
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
 };
